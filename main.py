4529d434f0df081511f52b1562a1a1b58bb388c0
fetchingg <nastya.zhdanova.2009@gmail.com>
2026-04-27 18:48:25 +0300

    Add files via upload
    add task1

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<title>')
+def index(title="Главная"):
+    return render_template('base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
